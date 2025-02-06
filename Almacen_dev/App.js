757a304f42bbddf306ac341ee1c7e88d0c278417
paolopuga <ppugawork@gmail.com>
2025-02-06 11:17:31 +0100

    packages

--- Almacen_dev/App.js
+++ Almacen_dev/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableHighlight, Pressable, Image } from 'react-native';
 
 export default function App() {
-  return (
+  (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Text>Almacen golfo!</Text>
+      <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}} style= {styles.imagen} />
+      <Button title="Press me" onPress={() => alert('Button pressed')} />
+      <TouchableHighlight onPress={() => alert('TouchableHighlight pressed')}>
+        <Text>TouchableHighlight</Text>
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +22,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  imagen: {
+    width: 200,
+    height: 200,
   },
 });
