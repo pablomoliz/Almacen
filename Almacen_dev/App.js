e7992e37fab7bed7bc11429f0b9594cb182aebe0
paolopuga <ppugawork@gmail.com>
2025-02-06 23:33:48 +0100

    prettierEsLint

--- Almacen_dev/App.js
+++ Almacen_dev/App.js
@@ -1,17 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TouchableHighlight, Pressable, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+  Image,
+} from "react-native";
 
 export default function App() {
-  (
+  return (
     <View style={styles.container}>
-      
       <Text>Almacen golfo!</Text>
-      <Image source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}} style= {styles.imagen} />
-      <Button title="Press me" onPress={() => alert('Button pressed')} />
-      <TouchableHighlight onPress={() => alert('TouchableHighlight pressed')}>
+      <Image
+        source={{ uri: "https://reactnative.dev/docs/assets/p_cat2.png" }}
+        style={styles.imagen}
+      />
+      <Button title="Press me" onPress={() => alert("Button pressed")} />
+      <TouchableHighlight onPress={() => alert("TouchableHighlight pressed")}>
         <Text>TouchableHighlight</Text>
       </TouchableHighlight>
       <StatusBar style="auto" />
+      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/icon.png")} style={styles.imagen} />
     </View>
   );
 }
@@ -19,9 +30,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
   imagen: {
